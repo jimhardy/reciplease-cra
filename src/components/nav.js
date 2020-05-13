76a11a5a81a71358d41603be0014fe218fd53f60
James Hardy <jamesthardy@gmail.com>
2020-05-13 21:43:56 +0100

    adding navbar

---
+++ src/components/nav.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
+import { Tab, Tabs } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+  },
+}));
+function Nav() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <RestaurantIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Reciplease
+          </Typography>
+          <Tabs>
+            <Tab label="Get Recipe" />
+            <Tab label="Pantry" />
+            <Tab label="Meal Planner" />
+          </Tabs>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Nav;
